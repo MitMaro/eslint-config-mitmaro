42a729ec32300add3fd95eada4dc375820d33fbc
Tim Oram <dev@mitmaro.ca>
2018-07-28 12:32:18 -0230

    Add support for chai-expect plugin

---
+++ rules/plugins/chai-expect.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+	rules: {
+		'chai-expect/no-inner-compare': 'error',
+		'chai-expect/missing-assertion': 'error',
+		'chai-expect/terminating-properties': 'error',
+	},
+};
